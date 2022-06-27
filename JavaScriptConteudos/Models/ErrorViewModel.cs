e74c6cf970bebfc699e25212afcd38b5c2f23018
Relinton <relinton.pinheiro.infouniron@gmail.com>
2022-06-27 14:00:28 -0400

    Conteúdo Prático em JavaScript

---
+++ JavaScriptConteudos/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace JavaScriptConteudos.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
